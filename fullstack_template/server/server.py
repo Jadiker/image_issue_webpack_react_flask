928cdd5b640ee66e0c1457d66bed1e0a6d26cb44
London Lowmanstone <happylondon4@gmail.com>
2019-01-20 15:22:43 -0500

    Trying @Joost's new solution

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -3,9 +3,9 @@ import random
 from flask import Flask, render_template, send_from_directory
 
 # app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
-# app = Flask(__name__, static_folder="../static") # TemplateNotFound (index.html)
+app = Flask(__name__, static_folder="../static") # TemplateNotFound (index.html)
 # app = Flask(__name__, root_path="../static") # TemplateNotFound (index.html)
-app = Flask(__name__, static_folder="../static", template_folder="../static")
+# app = Flask(__name__, static_folder="../static")
 
 def get_hello():
     greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
@@ -29,13 +29,13 @@ def hello():
 #     print("getting something from dist: {}".format(path))
 #     return send_from_directory("./dist", path)
     
-@app.route('/<path:path>')
-def static_file(path):
-    print("getting static file {}".format(path))
-    if path.startswith("public/"):
-        path = "dist/" + path
-        print("in public folder; path changed to: {}".format(path))
-    return app.send_static_file(path)
+# @app.route('/<path:path>')
+# def static_file(path):
+#     print("getting static file {}".format(path))
+#     if path.startswith("public/"):
+#         path = "dist/" + path
+#         print("in public folder; path changed to: {}".format(path))
+#     return app.send_static_file(path)
 
 
 if __name__ == "__main__":
